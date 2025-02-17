b476ea93ac82806e3dbf1ba74878d0692dd10e88
Murtaza Hatim <murtazahatimr@icloud.com>
2025-02-17 14:18:41 +0300

    Open port on 0.0.0.0 instead of localhost.

--- app.py
+++ app.py
@@ -245,4 +245,5 @@ def process_file():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
